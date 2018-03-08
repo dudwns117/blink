f15fa43a3faae2ee283b7fdd87f14de8ebf49517
dudwns117 <37110698+dudwns117@users.noreply.github.com>
2018-03-08 21:11:22 +0900

    Add files via upload

---
+++ Blink.cpp
@@ -0,0 +1,16 @@
+#include "Arduino.h"
+#include "Blink.h"
+
+Blink::Blink(int num){
+  pinMode(num, OUTPUT);
+  _num = num;
+}
+
+void Blink::on(int t){
+  digitalWrite(_num, HIGH);
+  delay(t);
+}
+void Blink::off(int t){
+  digitalWrite(_num, LOW);
+  delay(t);
+}
---
+++ Blink.h
@@ -0,0 +1,15 @@
+#ifndef Blink_h
+#define Blink_h
+
+#include "Arduino.h"
+
+class Blink{
+  public:
+    Blink(int num);
+    void on(int t);
+    void off(int t);
+   private:
+    int _num;
+};
+
+#endif
